039f3567d4ac1dd835d55f66b4bcb2492e15c93e
chipmunkcol <jack95711@gmail.com>
2023-04-27 18:45:15 +0900

    환경설정 조정

--- server.js
+++ server.js
@@ -1,35 +1,38 @@
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({extended : true}));
-app.set('view engine', 'ejs')
+// const bodyParser = require('body-parser');
+// require('dotenv').config()
 
-const { MongoClient, ServerApiVersion, Db, Collection } = require('mongodb');
-const uri = "mongodb+srv://nodejs:q1w2e3r4@cluster0.e9z3hma.mongodb.net/?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-const DB = client.db('todoApp')
+// app.use(bodyParser.urlencoded({extended : true}));
+// app.set('view engine', 'ejs')
 
-client.connect(err => {
-    console.log('err: ', err);
-    client.close();
+// const { MongoClient, ServerApiVersion, Db, Collection } = require('mongodb');
+// const uri = process.env.DB_API;
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+// const DB = client.db('todoApp')
+
+// client.connect(err => {
+//     console.log('err: ', err);
+//     client.close();
     
-}).then(()=>{console.log('hello, mongoDB!')});
+// }).then(()=>{console.log('hello, mongoDB!')});
+
+// app.post('/submit', async(req, res) => {
+//     // console.log(req.body.todo);
+//     const postDB = await DB.collection('post').insertOne({todo: req.body.todo, due: req.body.due})
+//     console.log('몽고디비 저장완료', postDB)
+// });
+
+// app.get('/list', async(req, res) => {
+
+//     const getDB = await DB.collection('post').find().toArray();
+//     console.log(getDB)
+
+//     res.render('list.ejs', { getDB });
+// });
+
 
 app.listen(3000, ()=>{console.log('listening on 3000')});
 
 app.get('/', (req, res) => { res.sendFile(__dirname + '/index.html')});
 app.get('/write', (req, res) => {res.sendFile(__dirname + '/write.html')});
-
-app.post('/submit', async(req, res) => {
-    // console.log(req.body.todo);
-    const postDB = await DB.collection('post').insertOne({todo: req.body.todo, due: req.body.due})
-    console.log('몽고디비 저장완료', postDB)
-});
-
-app.get('/list', async(req, res) => {
-
-    const getDB = await DB.collection('post').find().toArray();
-    console.log(getDB)
-
-    res.render('list.ejs', { getDB });
-});
